0e589d33cb0992a421d9748473433f36e2a031a7
Monsoon Co <monsoonco@Monsoons-MacBook-Pro.local>
2018-05-07 18:33:50 -0700

    created the html and css files added elements

---
+++ train.js
@@ -0,0 +1,29 @@
+alert(1)
+
+
+
+!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+
+// firebase java script link 
+
+<script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCkxdFfRLw-S3QZXdUxTKH8kljp0S1iRPw",
+    authDomain: "trainscheduler87.firebaseapp.com",
+    databaseURL: "https://trainscheduler87.firebaseio.com",
+    projectId: "trainscheduler87",
+    storageBucket: "trainscheduler87.appspot.com",
+    messagingSenderId: "989026000813"
+  };
+  firebase.initializeApp(config);
+
+//   variable names
+
+var trainName
+var destination
+va
